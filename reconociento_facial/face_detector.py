0b5fb0bbc51aa73a8137f2834da63b953b06dff2
Agustin Raggi <agustinraggi@gmail.com>
2022-08-28 21:13:53 -0300

    ia.2.0

--- reconociento_facial/face_detector.py
+++ reconociento_facial/face_detector.py
@@ -1,9 +1,10 @@
-from asyncore import read
-from sre_constants import SUCCESS
-from tkinter import Frame
 import cv2
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+eye_detector = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 video = cv2.VideoCapture(0) 
 
@@ -15,11 +16,22 @@ while True:
 
         break
     Frame_grayscale = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+
     faces = face_detector.detectMultiScale(Frame_grayscale)
     
+    smiles = smile_detector.detectMultiScale(Frame_grayscale, scaleFactor = 1.7, minNeighbors = 20)
+
+    eyes = eye_detector.detectMultiScale(Frame_grayscale)
+
     for (x, y, w, h) in faces:
         cv2.rectangle(Frame, (x, y), (x+w, y+h), (100,200,50), 2)
-    cv2.imshow("walter", Frame)
+
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(Frame, (x, y), (x+w, y+h), (0,0,255), 2)
+
+    for (x, y, w, h) in smiles:
+        cv2.rectangle(Frame, (x,y), (x+w, y+h), (255,255,255), 2 )
+    cv2.imshow("Rosalia", Frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
@@ -28,4 +40,3 @@ video.release()
 cv2.destroyAllWindows()
 
 print("esta funcionando!")
-
