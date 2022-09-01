2ee007e0056512a55097090678c591adbcd44bca
agustinraggi <agustinraggi@gmail.com>
2022-08-31 22:28:16 -0300

    se agrego el reconocimiento facial 3

--- reconociento_facial/face_detector.py
+++ reconociento_facial/face_detector.py
@@ -1,11 +1,12 @@
-from asyncore import read
-from sre_constants import SUCCESS
-from tkinter import Frame
 import cv2
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-video = cv2.VideoCapture(0) 
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+eye_detector = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+video = cv2.VideoCapture("28003-1631171950.webp") 
 
 while True:
 
@@ -15,17 +16,27 @@ while True:
 
         break
     Frame_grayscale = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+
     faces = face_detector.detectMultiScale(Frame_grayscale)
     
+    smiles = smile_detector.detectMultiScale(Frame_grayscale, scaleFactor = 1.7, minNeighbors = 20)
+
+    eyes = eye_detector.detectMultiScale(Frame_grayscale)
+
     for (x, y, w, h) in faces:
         cv2.rectangle(Frame, (x, y), (x+w, y+h), (100,200,50), 2)
-    cv2.imshow("walter", Frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(Frame, (x, y), (x+w, y+h), (0,0,255), 2)
+
+    for (x, y, w, h) in smiles:
+        cv2.rectangle(Frame, (x,y), (x+w, y+h), (255,255,255), 2 )
+    cv2.imshow("Rosalia", Frame)
+
+    if cv2.waitKey(0) & 0xFF == ord("1"):
         break
 
 video.release()
 cv2.destroyAllWindows()
 
 print("esta funcionando!")
-
